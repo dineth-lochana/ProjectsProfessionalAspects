1971b6e62da7ba7a1aa96acb12c36cc0805aef8b
Dhananjaya-Dilshan <dhananjayad032@gmail.com>
2024-04-04 15:30:13 +0530

    modify admin dashbord
    add quotation

--- client/src/pages/dhananjaya/AdminDashbord.jsx
+++ client/src/pages/dhananjaya/AdminDashbord.jsx
@@ -1,59 +1,86 @@
-import React from 'react';
+import React from "react";
 
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const AdminDashboard = () => {
-  
   return (
-    <div >
-<p style={{ textAlign: 'center', paddingTop: '100px', fontSize: '50px' }}>Admin Dashboard</p>
+    <div>
+      <p style={{ textAlign: "center", paddingTop: "100px", fontSize: "50px" }}>
+        Admin Dashboard
+      </p>
 
-    <div className='menu' style={{ paddingTop: '00px' ,paddingBottom:'100px'}}>
-    
+      <div
+        className="menu"
+        style={{ paddingTop: "00px", paddingBottom: "100px" }}
+      >
+        <div className="products">
+          <Link to="/AddProduct">
+            {" "}
+            <div className="mproduct">
+              <div className="image">
+                <img src="./images/add.png" alt="T-SHIRTS" />
+              </div>
+              <div className="name">
+                <h3>Add Product</h3>
+              </div>
+            </div>
+          </Link>
 
-    <div className="products">
-    <Link to="/AddProduct">    <div className="mproduct">
-      
-      <div className="image">
-      
-        <img src="./images/add.png" alt="T-SHIRTS" />
-      </div>
-      <div className="name">
-        <h3>Add Product</h3>
-      </div>
-    </div></Link> 
-  
-    <Link to="/Product">    
-      <div className="mproduct">
-        <div className="image">
-          <img src="./images/view.png" alt="MUGS" style={{ width: '350px', paddingTop: '10px' }} />
+          <Link to="/Product">
+            <div className="mproduct">
+              <div className="image">
+                <img
+                  src="./images/view.png"
+                  alt="MUGS"
+                  style={{ width: "350px", paddingTop: "10px" }}
+                />
+              </div>
+              <div className="name">
+                <h3>View Products</h3>
+              </div>
+            </div>
+          </Link>
+          <Link to="/manageAcounts">
+            <div className="mproduct">
+              <div className="image">
+                <img
+                  src="./images/acounts.png"
+                  alt="PHONE CASES"
+                  style={{
+                    width: "320px",
+                    paddingTop: "10px",
+                    paddingLeft: "10px",
+                  }}
+                />
+              </div>
+              <div className="name">
+                <br></br>
+                <h3>Manage Acounts</h3>
+              </div>
+            </div>
+          </Link>
+          <Link to="/Quotation">
+            <div className="mproduct">
+              <div className="image">
+                <img
+                  src="./images/quotation.png"
+                  alt="PHONE CASES"
+                  style={{
+                    width: "350px",
+                    paddingTop: "10px",
+                    paddingLeft: "10px",
+                  }}
+                />
+              </div>
+              <div className="name">
+                <br></br>
+                <h3>Quotation</h3>
+              </div>
+            </div>
+          </Link>
         </div>
-        <div className="name">
-          <h3>View Products</h3>
-        </div>
       </div>
-      </Link>
-      <Link to="/manageAcounts">
-      <div className="mproduct">
-        <div className="image">
-          <img
-            src="./images/acounts.png"
-            alt="PHONE CASES"
-            style={{ width: '320px', paddingTop: '10px', paddingLeft: '10px' }}
-          />
-        </div>
-        <div className="name">
-          <br></br>
-          <h3>Manage Acounts</h3>
-        </div>
-      </div>
-      </Link>
     </div>
-    
-    </div>
-    </div>
-    
   );
 };
 
---
+++ client/src/pages/shehan/quotation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Quotation = () => {
+  return (
+    <div>
+      <p style={{ textAlign: "center", paddingTop: "100px", fontSize: "50px" }}>
+      Quotations
+      </p>
+
+      
+    </div>
+  );
+};
+
+export default Quotation;
